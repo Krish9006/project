97d29bfd34228629717777c77ddb584c6d93b7a0
krish <guptashanu341@gmail.com>
2025-02-23 23:41:19 +0530

    9

--- SnkaeGame.js/Todo app/script.js
+++ SnkaeGame.js/Todo app/script.js
@@ -2,31 +2,36 @@ let tasks = [];
 let input = document.querySelector("#input");
 let button = document.querySelector("#add");
 let addtask = document.querySelector("#tasklist");
-
-button.addEventListener("click", function() {
+button.addEventListener("click", addTask);
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+function addTask() {
     let newtask = input.value.trim();
-    if(newtask) {
+    if (newtask) {
         tasks.push(newtask);
         input.value = "";
         displaytasks();
     }
-});
-
+}
 function displaytasks() {
-    addtask.innerHTML = ""; 
+    addtask.innerHTML = "";
     tasks.forEach(function(task, index) {
-        let li = document.createElement("li"); 
-        li.textContent = task; 
+        let li = document.createElement("li");
+        li.textContent = task;
         let delButton = document.createElement("button");
         delButton.textContent = "Delete";
+        delButton.classList.add("delete-button");
         delButton.addEventListener("click", function() {
-            deletetask(index); 
+            deletetask(index);
         });
         li.appendChild(delButton);
         addtask.appendChild(li);
     });
 }
 function deletetask(index) {
-    tasks.splice(index, 1); 
-    displaytasks(); 
+    tasks.splice(index, 1);
+    displaytasks();
 }
